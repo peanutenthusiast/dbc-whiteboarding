b6ab66abc1a5120ce44c56831998f72a319c043a
peanutenthusiast <mhdedios@gmail.com>
2017-11-16 09:36:22 -0800

    Add initial solution for even_fib_sum algorithm

--- July_19/even_fibonnaci_numbers.rb
+++ July_19/even_fibonnaci_numbers.rb
@@ -1,3 +1,9 @@
 def even_fib_sum
   #your code here
+  fib_seq = [1, 2]
+  while fib_seq[-1] < 4000000
+    fib_seq << fib_seq[-1] + fib_seq[-2]
+  end
+  fib_seq.reduce(0) {|even_sum, num| even_sum += num % 2 == 0 ? num : 0}
 end
+
